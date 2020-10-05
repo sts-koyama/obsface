5f7042abef669d30e88be1c19f9daee3a09520fc
sts-koyama <koyamasatoshi.3@gmail.com>
2020-10-05 22:07:25 +0900

    fix create

--- obsface/settings.py
+++ obsface/settings.py
@@ -142,9 +142,10 @@ except ImportError:
     pass
 
 # secret key
-SECRET_KEY = os.environ["SECRET_KEY"]
-import django_heroku
-django_heroku.settings(locals())
+if not DEBUG:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    import django_heroku
+    django_heroku.settings(locals())
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
